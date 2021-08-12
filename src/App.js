0f0e5894e9cd2dde5ce2137e3bc095c10e919ffe
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-08-12 17:55:35 -0300

    Inicializando Requisito 12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import FinalizingPurchase from './Pages/FinalizingPurchase';
 import Home from './Pages/Home';
 import ProductDetails from './Pages/ProductDetails';
 import ShoppingCart from './Pages/ShoppingCart';
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Route exact path="/" component={ Home } />
         <Route exact path="/shopping-cart" component={ ShoppingCart } />
         <Route path="/product-details/:id" component={ ProductDetails } />
+        <Route path="/finalizing-purchase" component={ FinalizingPurchase } />
       </BrowserRouter>
     );
   }
